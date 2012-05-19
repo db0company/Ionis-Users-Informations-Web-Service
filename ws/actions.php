79fa0eab1a47fcaea5f3b593a38e7869d1de3a5f
db0 <db0company@gmail.com>
2012-05-19 17:27:57 +0200

    [ws] Get all informations with a single request

--- ws/actions.php
+++ ws/actions.php
@@ -150,6 +150,26 @@ function	ws_get_plan($result)
   return ($result);
 }
 
+function	ws_get_infos($result)
+{
+  global	$iui;
+
+  if (!(check_params($result, array('login'))))
+    return ($result);
+  $result['result']['login'] = $_GET['login'];
+  $result['result']['uid'] = ($iui->getUid($_GET['login']));
+  $result['result']['name'] = ($iui->getName($_GET['login']));
+  $result['result']['group'] = ($iui->getGroup($_GET['login']));
+  $result['result']['school'] = ($iui->getSchool($_GET['login']));
+  $result['result']['promo'] = ($iui->getPromo($_GET['login']));
+  $result['result']['city'] = ($iui->getCity($_GET['login']));
+  $result['result']['report_url'] = ($iui->getReportUrl($_GET['login']));
+  $result['result']['photo_url'] = ($iui->getPhotoUrl($_GET['login']));
+  $result['result']['plan'] = ($iui->getPlan($_GET['login'],
+					     'plan'));
+  return ($result);
+}
+
 function	ws_search($result)
 {
   global	$iui;
