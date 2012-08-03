4f5b3b70d983139b5086086e10132c84aa9b631e
db0 <db0company@gmail.com>
2012-08-03 17:54:16 +0200

    Changing conf parameters

--- ws/conf.php
+++ ws/conf.php
@@ -1,8 +1,9 @@
 <?php
 
 // Type this on your terminal to get the required file :
-// $ wget "https://raw.github.com/db0company/Ionis-Users-Informations/master/ionisinfosql.class.php"
-$class_path = '.';
+// $ wget "https://raw.github.com/db0company/Ionis-Users-Informations/master/ionisinfo.class.php"
+
+$ionisusersinformations_path = 'ionisinfo.class.php';
 
 $mysql_login = 'foo';
 $mysql_pass = 'bar';
@@ -11,9 +12,9 @@ $mysql_pass = 'bar';
 $mysql_dbname = 'my_database';
 
 $ionis_login = 'exampl_e';
-$ionis_pass = '2q4xfcc3';
-$intra_pass = 'Dbtmh/(x';
+$ionis_unix_password = '2q4xfcc3';
+$intra_ppp_pass = 'Dbtmh/(x';
 
-$path_local_files = '.';
+$absolute_path_local_files = '.';
 
 $afs = false;
--- ws/load.php
+++ ws/load.php
@@ -1,8 +1,8 @@
 <?php
 
 include_once('conf.php');
-include_once($class_path.'/ionisinfo.class.php');
+include_once($ionisusersinformations_path);
 $iui = new IonisInfo($mysql_login, $mysql_pass, $mysql_dbname,
-		     $ionis_login, $ionis_unix_password, $path_local_files, $afs,
-		     $intra_pass);
+		     $ionis_login, $ionis_unix_password, $absolute_path_local_files, $afs,
+		     $ionis_ppp_pass);
 
