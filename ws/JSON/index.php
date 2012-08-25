4be580e74c9a3959b0d5436c2ff75553db372b99
db0 <db0company@gmail.com>
2012-08-25 16:41:51 +0200

    JSON generated with json_encode

--- ws/JSON/index.php
+++ ws/JSON/index.php
@@ -3,32 +3,4 @@
 $format = 'ini';
 include_once('../ws.php');
 
-echo '{',
-  "\n",
-  '  "action"="',
-  $result['action'],
-  '",',
-  "\n",
-  '  "error"="',
-  $result['error'],
-  '",',
-  "\n",
-  '  "result"=',
-  "\n",
-  '  {',
-  "\n";
-
-$flag = false;
-foreach($result['result'] as $key => $value)
-{
-  if ($flag)
-    echo ',', "\n";
-  echo '    "', $key, '":"', $value, '"';
-  $flag = true;
-}
-
-echo  "\n",
-  '  }',
-  "\n",
-  '}',
-  "\n";
+echo json_encode(array_reverse($result));
